5739bba157585bba5c58c26626c03fff38d6a4a7
sadv1r <5000487+sadv1r@users.noreply.github.com>
2026-03-28 04:31:12 +0300

    Update ESLint config to disable specific icon validation rules
    https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/#step-2-add-an-icon

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import { config } from '@n8n/node-cli/eslint';
 
-export default config;
+export default [
+	...config,
+	{
+		rules: {
+			'n8n-nodes-base/node-class-description-icon-not-svg': 'off',
+			'@n8n/community-nodes/icon-validation': 'off',
+		},
+	},
+];
